134c6f11ec39d51cfbe5bbf0b4481b5302a0cae4
Crashpilot1000 <r.balko@yahoo.de>
2014-08-20 07:04:29 +0200

    Sensors.c px4 license deleted for a reason
    PX4 "Copyright" removed since it is ripped off here: http://imaginaryz.blogspot.de/2011/04/least-squares-fit-sphere-to-3d-data.html

--- src/sensors.c
+++ src/sensors.c
@@ -525,42 +525,10 @@ static void Mag_Calibration(void)                                 // Called from
 #endif
 
 /****************************************************************************
- *
- *   Copyright (C) 2012 PX4 Development Team. All rights reserved.
- *   Author: Petri Tanskanen <petri.tanskanen@inf.ethz.ch>
- *           Lorenz Meier <lm@inf.ethz.ch>
- *           Thomas Gubler <thomasgubler@student.ethz.ch>
- *           Julian Oes <joes@student.ethz.ch>
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- * 3. Neither the name PX4 nor the names of its contributors may be
- *    used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
  * Crashpilot note: Parameter slightly changed. Size to float.
- * Docu is here: https://pixhawk.ethz.ch/px4/docs/calibration__routines_8cpp.html
+ * PX4 "Copyright" removed since it is ripped off here:
+ * http://imaginaryz.blogspot.de/2011/04/least-squares-fit-sphere-to-3d-data.html
+ * PX4 Docu: https://pixhawk.ethz.ch/px4/docs/calibration__routines_8cpp.html
  ****************************************************************************/
 #define sflsdelta 0.0f
 #define maxiterations 100
